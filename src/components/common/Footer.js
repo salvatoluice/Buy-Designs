6ed5c4e59701474f51e70bb34ff5afe5f34a0dc9
Salvato Luice <salvatoluice5@gmai.com>
2023-01-22 15:09:50 +0300

    Add Commons

---
+++ src/components/common/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Copyright © Salvato all rights reserved. Developed by Salvato</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
